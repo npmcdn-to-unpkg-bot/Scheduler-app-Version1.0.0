60a2bfb3ad7e26b14f9e34a7db5a1e5295073f1c
mangapi <aim_ahmad@hotmail.com>
2016-05-09 13:02:15 +0200

    adding untracked files

---
+++ SchedulerWebApp - Continued/SchedulerWebApp/Models/PostalEmail/PasswordResetEmail.cs
@@ -0,0 +1,13 @@
+﻿using Postal;
+
+namespace SchedulerWebApp.Models.PostalEmail
+{
+    public class PasswordResetEmail : Email
+    {
+        public string ReceiverEmail { get; set; }
+        public string ReceiverName { get; set; }
+        public string AdminEmail { get; set; }
+        public string EmailSubject { get; set; }
+        public string PassWordRestLink { get; set; }
+    }
+}
