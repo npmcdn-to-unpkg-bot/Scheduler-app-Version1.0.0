5e5f5d4cc31d96fc1d849a30943271026acdf834
mangapi <aim_ahmad@hotmail.com>
2016-09-01 14:04:27 +0200

    new file added

---
+++ SchedulerWebApp - Continued/SchedulerWebApp/Models/ValidationAttributes/ConvertDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchedulerWebApp.Models.ValidationAttributes
+{
+    public  static class ConvertDateTime
+    {
+        public static DateTime ToSwissTimezone(DateTime dateTimeNowUtc)
+        {
+            var swissTimezone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            var currentSwissTime = TimeZoneInfo.ConvertTime(dateTimeNowUtc, swissTimezone);
+            return currentSwissTime;
+        }
+    }
+}
